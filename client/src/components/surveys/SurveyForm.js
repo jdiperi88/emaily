753a30d1047662031ce93ebdd84a2a1c69044239
jdiperi88 <jdiperi88@yahoo.com>
2018-11-16 09:19:11 -0500

    added SurveyFormReview componenet

--- client/src/components/surveys/SurveyForm.js
+++ client/src/components/surveys/SurveyForm.js
@@ -28,9 +28,7 @@ class SurveyForm extends Component {
 		return (
 			<div>
 				<form
-					onSubmit={this.props.handleSubmit(values =>
-						console.log("test", values)
-					)}
+					onSubmit={this.props.handleSubmit(() => this.props.onSurveySubmit())}
 				>
 					{this.renderFields()}
 					<Link to="/surveys" className="red btn-flat white-text">
